b86302ef3c19be2fa7631ca6e4362e94b87045ec
Alabi Jeremiah <alabijeremiah955@gmail.com>
2026-04-30 06:33:33 -0700

    updated my project1

--- myproject1/script.js
+++ myproject1/script.js
@@ -6,3 +6,21 @@ function toggleNav() {
   nav.classList.toggle("hide-nav");
 }
 
+const animatedItems = document.querySelectorAll('.animate, .animation, .animate2');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('in-view');
+    } else {
+      entry.target.classList.remove('in-view');
+    }
+  });
+}, {
+  root: null,
+  rootMargin: '0px 0px -10% 0px',
+  threshold: 0.15
+});
+
+animatedItems.forEach(item => observer.observe(item));
+
